4b5ff4cb3b556c76d1a044139ff29c2c61a924bf
Oliver Schulz <oschulz@mpp.mpg.de>
2021-05-12 10:48:15 +0200

    Add dependencies

--- src/ForwardDiffPullbacks.jl
+++ src/ForwardDiffPullbacks.jl
@@ -5,10 +5,24 @@ __precompile__(true)
 """
     ForwardDiffPullbacks
 
-Template for Julia packages.
+ChainRulesCore compatible pullbacks using ForwardDiff.
 """
 module ForwardDiffPullbacks
 
+using LinearAlgebra
+
+import ChainRulesCore
+import ForwardDiff
+import StaticArrays
+
+using Requires
+
 include("hello_world.jl")
+
+function __init__()
+    # @require Nabla = "49c96f43-aa6d-5a04-a506-44c7070ebe78" include("nabla_support.jl")
+    # @require ReverseDiff = "37e2e3b7-166d-5795-8a7a-e32c996b4267" include("reversediff_support.jl")
+    # @require Tracker = "9f7883ad-71c0-57eb-9f7f-b5c9e6d3789c" include("tracker_support.jl")
+end
 
 end # module
